c1ca25cd33c723bba9a1afb76e0df02c67368a5b
alireza Programmer <74681914+alirezaprogrammermaker@users.noreply.github.com>
2021-07-25 00:44:46 +0430

    Update file.py

--- file.py
+++ file.py
@@ -8,9 +8,11 @@ headers = {
     'cache-control': "no-cache",
     'postman-token': "06c46068-fbcd-61d6-ad7f-6399b65001a3"
     }
+i = 1
+while i < 200:
+  print(i)
+  response = requests.request("POST", url, data=payload, headers=headers)
+  print(response.text)
+  i += 1
 
-response = requests.request("POST", url, data=payload, headers=headers)
 
-print(response.text)
-
-print(" ")
